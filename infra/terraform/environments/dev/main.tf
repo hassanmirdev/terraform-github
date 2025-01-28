34d5b851e957cce45e3e374ec235eb8cc071f112
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-01-29 00:03:17 +0530

    Update main.tf

--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -2,20 +2,20 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "vpc" {
-  source      = "../common"
-  vpc_cidr    = var.vpc_cidr
-  subnet_cidr = var.subnet_cidr
-}
+# module "vpc" {
+ # source      = "../common"
+ # vpc_cidr    = var.vpc_cidr
+ # subnet_cidr = var.subnet_cidr
+#}
 
 module "ec2_instance" {
   source      = "../common"
-  ami         = var.ami
+  ami_value   = var.ami
   instance_type = var.instance_type
 }
 
-module "rds" {
-  source       = "../common"
-  db_username  = var.db_username
-  db_password  = var.db_password
-}
+# module "rds" {
+ # source       = "../common"
+ # db_username  = var.db_username
+ # db_password  = var.db_password
+#}
