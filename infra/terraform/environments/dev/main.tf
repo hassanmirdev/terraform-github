5211801449c750b21e9ebc6bbdda2870d62645ae
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-01-28 23:03:23 +0530

    Update main.tf

--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 module "vpc" {
-  source     = "../common"
-  vpc_cidr   = var.vpc_cidr
+  source      = "../common"
+  vpc_cidr    = var.vpc_cidr
   subnet_cidr = var.subnet_cidr
 }
 
