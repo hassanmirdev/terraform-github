ee8595bfe7b8784e652dcf633d363cb04e22e921
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-01-28 21:19:07 +0530

    Create rds.tf

---
+++ infra/terraform/environments/common/rds.tf
@@ -0,0 +1,11 @@
+resource "aws_db_instance" "example" {
+  allocated_storage     = 20
+  db_instance_class     = "db.t2.micro"
+  engine                = "mysql"
+  engine_version        = "8.0"
+  name                  = "exampledb"
+  username              = var.db_username
+  password              = var.db_password
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  db_subnet_group_name  = aws_db_subnet_group.main.id
+}
