62d0aa7a6149b0cf15b2a252c325fb9c2678550c
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-01-28 23:47:41 +0530

    Update main.tf

--- infra/terraform/environments/staging/main.tf
+++ infra/terraform/environments/staging/main.tf
@@ -1,21 +1,21 @@
 provider "aws" {
-  region = "us-east-1a"
+  region = "us-east-1"
 }
 
 module "vpc" {
-  source     = "../common/vpc"
+  source     = "../common"
   vpc_cidr   = var.vpc_cidr
   subnet_cidr = var.subnet_cidr
 }
 
 module "ec2" {
-  source       = "../common/ec2"
+  source       = "../common"
   ami      = var.ami
   instance_type = var.instance_type
 }
 
 module "rds" {
-  source       = "../common/rds"
+  source       = "../common"
   db_username  = "admin"
   db_password  = "password"
 }
