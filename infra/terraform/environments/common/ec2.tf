0fb20b1c2e18e0f6288d6e8dc7e85f53bc9ee24f
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-01-28 22:51:25 +0530

    Update ec2.tf

--- infra/terraform/environments/common/ec2.tf
+++ infra/terraform/environments/common/ec2.tf
@@ -1,9 +1,13 @@
-resource "aws_instance" "example" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.subnet_a.id
+provider "aws" {
+    region = "us-east-1"
+}
 
-  tags = {
-    Name = "ExampleInstance"
-  }
+resource "aws_instance" "example" {
+    ami = var.ami
+    instance_type = var.instance_type
+     subnet_id     = aws_subnet.subnet_a.id
+
+        tags = {
+                Name = "WebServer"
+          }
 }
