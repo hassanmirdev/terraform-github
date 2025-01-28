72288c9717275f87736b0a41d47745005f073347
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-01-28 21:34:45 +0530

    Update and rename file1 to variables.tf

---
+++ infra/terraform/environments/staging/variables.tf
@@ -0,0 +1,23 @@
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "subnet_cidr" {
+  type = string
+}
+
+variable "ami" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_password" {
+  type = string
+}
