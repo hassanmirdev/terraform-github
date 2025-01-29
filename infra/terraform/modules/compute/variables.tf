4d872140cd1822a18fb4503a80e194f7d6edc63e
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-01-29 10:33:27 +0530

    Update variables.tf

--- infra/terraform/modules/compute/variables.tf
+++ infra/terraform/modules/compute/variables.tf
@@ -1,9 +1,9 @@
 variable "ami" {
-  type = "string"
+  type = string
   description = "This is AMI for the instance"
 }
 
 variable "instance_type" {
-  type = "string"
+  type = string
   description = "This is the instance type, for example: t2.micro"
 }
