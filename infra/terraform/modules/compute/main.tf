c7ffc972d00cec4871cad25a97e9cf0d79dfd344
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-01-29 10:46:20 +0530

    Update main.tf

--- infra/terraform/modules/compute/main.tf
+++ infra/terraform/modules/compute/main.tf
@@ -1,12 +1,16 @@
-terraform {
-  required_version = ">= 1.5.0"
+ # terraform {
+ # required_version = ">= 1.5.0"
  
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+ # required_providers {
+ #   aws = {
+#    source  = "hashicorp/aws"
+#      version = "~> 5.0"
+ #   }
+ # }
+# }
+
+terraform {
+  required_version = ">= 1.0" 
 }
 provider "aws" {
     region = "us-east-1"
