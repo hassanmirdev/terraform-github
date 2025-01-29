09d0b0f1ba674f1a259712bd993f4ea0d7700b86
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-01-29 10:03:12 +0530

    Create variables.tf

---
+++ infra/terraform/modules/network/variables.tf
@@ -0,0 +1,12 @@
+variable "vpc_cidr" {
+ description = "The CIDR block for the VPC"
+ type = string
+}
+variable "subnet_a_cidr" {
+ description = "CIDR block for Subnet A"
+ type = string
+}
+variable "subnet_b_cidr" {
+ description = "CIDR block for Subnet B"
+ type = string
+}
